6d1797f47e2b8bd439e2cd79ec3421522707acad
paulz97 <58420487+paulz97@users.noreply.github.com>
2019-12-02 10:55:20 +0100

    Add files via upload

---
+++ ue6_aufgabe2_zemann.py
@@ -0,0 +1,11 @@
+"""Fläche eines Rechtecks berechnen"""
+
+# Erklärung
+print('Berechnen der Fläche eines Rechtecks')
+
+# Länge und Breite eines Rechtecks abfragen.
+länge = int(input('Geben Sie bitte die Länge in cm ein!: '))
+breite = int(input('Geben Sie bitte die Breite in cm ein!: '))
+
+fläche = länge * breite
+print('Die Fläche beträgt: ' + str(fläche) + ' cm^2')
